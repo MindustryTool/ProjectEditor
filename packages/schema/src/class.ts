31713debdbd996386417e4aed6c0cc7e8e43b8fa
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-06-11 00:33:24 +0700

    a

--- packages/schema/src/class.ts
+++ packages/schema/src/class.ts
@@ -18,17 +18,44 @@ export const classSchema = <T extends string>(classTypes: readonly T[], defaultV
 				v.picklist(classTypes),
 			);
 
+type ClassExtends<T extends string> = v.ObjectSchema<
+	{
+		className: v.LiteralSchema<T, v.ErrorMessage<v.LiteralIssue> | undefined>;
+	},
+	v.ErrorMessage<v.ObjectIssue> | undefined
+>;
+
+export function extend<T extends string>(clazz: T): ClassExtends<T> {
+	return v.object({ className: v.literal(clazz) });
+}
+
 export class ClassMap<K extends string> {
-	constructor(private readonly map: Record<K, SchemaFn<v.ObjectSchema<v.ObjectEntries, v.ErrorMessage<v.ObjectIssue> | undefined>>>) {}
+	constructor(
+		private readonly map: Record<
+			K,
+			SchemaFn<v.ObjectSchema<v.ObjectEntries, v.ErrorMessage<v.ObjectIssue> | undefined>> | ClassExtends<K>
+		>,
+	) {}
 
 	get(object: unknown, context: ProjectContents) {
 		const result: v.ObjectEntries = {};
 		if (object && typeof object === "object" && "type" in object && typeof object.type === "string") {
 			const key = (object.type.slice(0, 1).toUpperCase() + object.type.slice(1)) as K;
 			try {
-				if (this.map[key]) {
-					Object.assign(result, this.map[key](context).entries);
-				}
+				const resoleKey = (key: K) => {
+					if (this.map[key]) {
+						const value = this.map[key];
+						if (typeof value === "function") {
+							Object.assign(result, value(context).entries);
+						} else {
+							resoleKey(value.entries.className.literal);
+						}
+					} else {
+                        throw new Error(`Class ${key} not found`);
+                    }
+				};
+
+				resoleKey(key);
 			} catch {
 				console.error(this.map[key]);
 			}
